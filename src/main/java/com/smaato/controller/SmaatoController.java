447d5c780e43cc5656361dd27b22343d95b355bb
venkateshvg1987 <96410553+venkateshvg1987@users.noreply.github.com>
2022-02-21 11:16:13 +0530

    Scheduler Enabled

--- src/main/java/com/smaato/controller/SmaatoController.java
+++ src/main/java/com/smaato/controller/SmaatoController.java
@@ -1,10 +1,16 @@
 package com.smaato.controller;
 
+import java.net.URI;
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +21,8 @@ import com.smaato.service.UserService;
 /**
  * @author Venkatesh
  * 
- * http://localhost:8090/api/smaato/accept?id=20&endpoint=http://localhost:8080/api/smaato/count
- * http://localhost:8080/api/smaato/accept?id=20&endpoint=http://localhost:8080/api/smaato/count
+ *         http://localhost:8090/api/smaato/accept?id=20&endpoint=http://localhost:8080/api/smaato/redirect
+ *         http://localhost:8080/api/smaato/accept?id=20&endpoint=http://localhost:8080/api/smaato/redirect
  *
  */
 @RestController
@@ -24,6 +30,8 @@ class SmaatoController {
 	private static final Logger log = Logger.getLogger(SmaatoController.class.getName());
 
 	private static final String SMAATO_USER = "smaatousers";
+
+	private static final String redirect_url = "http://localhost:8080/api/smaato/count";
 
 	@Autowired
 	RestTemplate restTemplete;
@@ -40,8 +48,8 @@ class SmaatoController {
 	 * @param id
 	 * @param endpoint
 	 * 
-	 * http://localhost:8080//api/smaato/accept?id=18&endpoint=http://localhost:8080/api/smaato/count
-	 *                 
+	 *                 http://localhost:8080//api/smaato/accept?id=18&endpoint=http://localhost:8080/api/smaato/redirect
+	 * 
 	 * @return String
 	 */
 	@RequestMapping(value = "/api/smaato/accept")
@@ -52,20 +60,33 @@ class SmaatoController {
 			response = "ok";
 		}
 		if (null != endpoint) {
-			log.info("endpoint values is " + endpoint);
-			log.info("Triggering " + endpoint);
-			Long count = restTemplete.getForObject(endpoint, Long.class);
+			String uri = endpoint + "?input=" + getCount();
+			log.info("endpoint values is " + uri);
+			log.info("Triggering " + uri);
+			Long count = restTemplete.getForObject(uri, Long.class);
 			log.info("Request completed for " + endpoint);
 			log.info("user count" + count);
 		}
 		return response;
 	}
 
+	@GetMapping(value = "/api/smaato/redirect")
+	public ResponseEntity<Void> redirect(@RequestParam Long input) {
+		log.info("unique count from the userlist :: " + input);
+		log.info("HTTP STATUS CODE for redirect url :: "
+				+ ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirect_url)).build());
+		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirect_url)).build();
+	}
+
 	/**
-	 * @return count
+	 * @return count and log the count for every one minute
 	 */
+
+	@Scheduled(fixedRate = 60000)
 	@RequestMapping(value = "/api/smaato/count")
 	public Long getCount() {
+		log.info("unique count from the userlist :: " + userService.count() + " Current time is :: "
+				+ Calendar.getInstance().getTime());
 		return userService.count();
 
 	}
