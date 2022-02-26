a6c4c0cd1fe104f0b726f2b6f362cd30b02cc97d
venkateshvg1987 <96410553+venkateshvg1987@users.noreply.github.com>
2022-02-27 01:03:54 +0530

    final commit
    final commit

--- src/main/java/com/smaato/controller/SmaatoController.java
+++ src/main/java/com/smaato/controller/SmaatoController.java
@@ -10,14 +10,18 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.smaato.model.User;
 import com.smaato.service.UserService;
 
 /**
@@ -84,6 +88,27 @@ class SmaatoController {
 		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirect_url)).build();
 	}
 
+	@PostMapping(value = "/api/smaato/accept", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	public String acceptuser(@RequestBody User user) throws InterruptedException {
+		String response = "FAILED";
+		log.info("POST Request User Details " + user.getId() + "  URL :: " + user.getUrl());
+		boolean isNewUser = userService.checkIfUserExists(user.getId());
+		if (isNewUser) {
+			response = "OK";
+		}
+		if (null != user.getUrl()) {
+			String uri = user.getUrl() + "?input=" + getCount();
+			log.debug("POST URL is " + uri);
+			log.info(" Thread  : " + Thread.currentThread().getName() + " is calling  " + uri);
+			Long count = restTemplete.getForObject(uri, Long.class);
+			Thread.sleep(1000L);
+			log.info(" Thread  : " + Thread.currentThread().getName() + " request completed for " + uri);
+			log.info(" New User Count :: " + count);
+		}
+		return response;
+	}
+
 	/**
 	 * @return count and log the count for every one minute
 	 */
@@ -103,11 +128,4 @@ class SmaatoController {
 
 	}
 
-	
-	@RequestMapping(value = "/api/smaato/clear")
-	public Long removeAllUsers() {
-		Duration duration1 = Duration.ofDays(7);
-		return userService.clear(duration1);
-
-	}
 }
---
+++ src/main/java/com/smaato/model/User.java
@@ -0,0 +1,24 @@
+package com.smaato.model;
+
+public class User {
+
+	private int id;
+	private String url;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
